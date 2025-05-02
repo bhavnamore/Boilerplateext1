6e472c3df8c6fb6100f2120ba9d8d1e992d137f8
bhavnamore <164911063+bhavnamore@users.noreply.github.com>
2025-05-02 20:00:34 +0530

    Update script.js

--- Exp2/script.js
+++ Exp2/script.js
@@ -1,6 +1,6 @@
 function getWeather() {
     const city = document.getElementById('city').value;
-    const apiKey = 'e229216823707bb0a5b5153f0ba2fe9e';  // Replace with your OpenWeatherMap API key
+    const apiKey = '<>';  // Replace with your OpenWeatherMap API key
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   
     const xhr = new XMLHttpRequest();
